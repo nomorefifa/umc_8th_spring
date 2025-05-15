582543793a3fcd804bef6c3852b31eb6d111e6b0
wnsdud <cccjj3829@gmail.com>
2025-05-15 12:41:19 +0900

    add member_mission check api

---
+++ 05_week/spring/src/main/java/umc/spring/repository/MemberMissionRepository/MemberMissionRepository.java
@@ -0,0 +1,11 @@
+package umc.spring.repository.MemberMissionRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.domain.mapping.MemberMission;
+
+import java.util.List;
+
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+    List<MemberMission> findByMemberIdAndStatus(Long memberId, MissionStatus status);
+}
