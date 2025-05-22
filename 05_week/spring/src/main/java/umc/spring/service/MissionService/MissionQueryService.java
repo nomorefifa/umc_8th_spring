ea1d62f92efe2ec982037a06d180bd5600651350
wnsdud <cccjj3829@gmail.com>
2025-05-22 18:56:27 +0900

    add feature #15

--- 05_week/spring/src/main/java/umc/spring/service/MissionService/MissionQueryService.java
+++ 05_week/spring/src/main/java/umc/spring/service/MissionService/MissionQueryService.java
@@ -2,7 +2,9 @@ package umc.spring.service.MissionService;
 
 import umc.spring.domain.enums.MissionStatus;
 import umc.spring.web.dto.MemberMissionResponseDTO;
+import umc.spring.web.dto.MissionResponseDTO;
 
 public interface MissionQueryService {
     MemberMissionResponseDTO.MissionListDTO getMissionList(Long memberId, MissionStatus status);
+    MissionResponseDTO.MissionPreviewListDTO getMissionsByStore(Long storeId, int page);
 }
--- 05_week/spring/src/main/java/umc/spring/service/MissionService/MissionQueryServiceImpl.java
+++ 05_week/spring/src/main/java/umc/spring/service/MissionService/MissionQueryServiceImpl.java
@@ -1,12 +1,18 @@
 package umc.spring.service.MissionService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import umc.spring.converter.MissionConverter;
 import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.MemberMission;
+import umc.spring.domain.mapping.Mission;
 import umc.spring.repository.MemberMissionRepository.MemberMissionRepository;
+import umc.spring.repository.MissionRepository.MissionRepository;
 import umc.spring.web.dto.MemberMissionResponseDTO;
+import umc.spring.web.dto.MissionResponseDTO;
 
 import java.util.List;
 
@@ -20,5 +26,14 @@ public class MissionQueryServiceImpl implements MissionQueryService {
     public MemberMissionResponseDTO.MissionListDTO getMissionList(Long memberId, MissionStatus status) {
         List<MemberMission> missions = memberMissionRepository.findByMemberIdAndStatus(memberId, status);
         return MissionConverter.toListDTO(missions);
+    }
+
+    private final MissionRepository missionRepository;
+
+    @Override
+    public MissionResponseDTO.MissionPreviewListDTO getMissionsByStore(Long storeId, int page) {
+        Pageable pageable = PageRequest.of(page, 5); // 페이지 크기 5
+        Page<Mission> missionPage = missionRepository.findAllByStoreId(storeId, pageable);
+        return MissionConverter.toStoreMissionPreviewListDTO(missionPage);
     }
 }
